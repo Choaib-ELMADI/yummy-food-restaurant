b843eb885453e606e8cac27ff6ceeb110a19d5a1
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-01-05 23:57:49 +0100

    About Implemented

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { BsPauseCircleFill, BsFillPlayCircleFill, BsCheckLg } from 'react-icons/bs';
 
 import './About.css';
 import { images } from '../../constants/index';
@@ -6,20 +8,52 @@ import { images } from '../../constants/index';
 
 
 const About = () => {
+  const [pause, setPause] = useState(false);
+  const vidRef = useRef(null);
+
+  const handlePlayVideo = () => {
+    if (pause) vidRef.current.pause();
+    else vidRef.current.play();
+  }
+
   return (
     <div id='About' className='app__about'>
-      <div className="app__about-video">
-        <div className="video-overlay">
-          <button onClick="">||</button>
+      <div className="app__about-left">
+        <div className="app__about-video">
+          <div className="video-overlay">
+            <button onClick={ () => {
+              handlePlayVideo();
+              setPause(!pause);
+            }}
+            >
+              { !pause ? <BsPauseCircleFill /> : <BsFillPlayCircleFill /> }
+            </button>
+          </div>
+          <video ref={ vidRef }  muted loop autoPlay>
+            <source src={ images.video__1 } type="video/mp4" />
+          </video>
         </div>
-        <video  muted loop autoPlay>
-          <source src={ images.video__1 } type="video/mp4" />
-        </video>
+        <h1>Travel To <span>Morocco</span>, The Land of Spices.</h1>
       </div>
-      <div className="app__about-items">
-        <div className="app__about-item">
-            
-        </div>
+      <div className="app__about-right">
+        <h1><span>We </span>Create the best foody product</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Dolores deleniti nihil dolore sed illo quo, 
+          amet corporis pariatur temporibus tempore voluptate 
+          rem nobis, obcaecati et? Vitae tempore consequatur
+          voluptates vel voluptate beatae dolorem libero quod
+          corrupti unde consequuntur dolor iure in, voluptas ut
+          sapiente accusantium dicta? Reprehenderit autem at distinctio!
+        </p>
+        <ul className='app__about-list'>
+          <li><BsCheckLg className='check' /> Lorem ipsum dolor sit amet consectetur elit.</li>
+          <li><BsCheckLg className='check' /> Lorem ipsum dolor sit amet consectetur.</li>
+          <li><BsCheckLg className='check' /> Lorem ipsum dolor, amet consectetur adipisicing elit.</li>
+        </ul>
+        <Link to="SearchBar" className='app__about-more'>
+          <button>See More</button>
+        </Link>
       </div>
     </div>
   );
