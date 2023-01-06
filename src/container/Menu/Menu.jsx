714c8219ca5ed4f06a10e82082624ac77a7bcc17
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-01-06 09:58:02 +0100

    Menu Implemented

--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { AiOutlineHeart } from 'react-icons/ai';
+import React, { useState } from 'react';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { ImCart } from 'react-icons/im';
 
 import './Menu.css';
 import { images } from '../../constants/index';
@@ -7,12 +8,29 @@ import { images } from '../../constants/index';
 
 
 const Menu = () => {
+  const [heart, setHeart] = useState(false);
+
+
   return (
     <div id='Menu' className='app__menu'>
+      <div className="app__menu-right">
+        <h1>We have <span>Delicious</span> food</h1>
+        <h1><span>Tasty</span> food in town</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, 
+          adipisicing elit. Aperiam ullam fugiat 
+          dolores nihil aliquam consectetur est 
+          illum? Optio ipsum labore placeat minus 
+          delectus, error, molestiae corporis totam 
+          commodi voluptas id esse unde recusandae 
+          quidem nihil accusantium. Commodi ad dicta sunt.
+        </p>
+      </div>
+
       <div className="app__menu-left">
 
         <div className="app__menu-item">
-          <button>|||</button>
+          <button><ImCart className='heart-cart' fontSize={ 22 } /></button>
           <div className="app__menu-item-img">
             <img src={ images.tajine__8 } alt='order' />
           </div>
@@ -22,14 +40,16 @@ const Menu = () => {
               <small>Medium heat</small>
               <h5>$24.00</h5>
             </div>
-            <div className="app__menu-item-fav">
-              <AiOutlineHeart className='app__menu-heart' />
+            <div onClick={ () => setHeart(!heart) } className="app__menu-item-fav">
+            { heart ? <AiOutlineHeart className='app__menu-heart' /> : 
+                <AiFillHeart className='app__menu-heart' />
+              }
             </div>
           </div>
         </div>
 
         <div className="app__menu-item">
-          <button>|||</button>
+          <button><ImCart className='heart-cart' fontSize={ 22 } /></button>
           <div className="app__menu-item-img">
             <img src={ images.tajine__6 } alt='order' />
           </div>
@@ -39,21 +59,13 @@ const Menu = () => {
               <small>Medium heat</small>
               <h5>$35.00</h5>
             </div>
-            <div className="app__menu-item-fav">
-              <AiOutlineHeart className='app__menu-heart' />
+            <div onClick={ () => setHeart(!heart) } className="app__menu-item-fav">
+              { heart ? <AiOutlineHeart className='app__menu-heart' /> : 
+                <AiFillHeart className='app__menu-heart' />
+              }
             </div>
           </div>
         </div>
-      </div>
-      <div className="app__menu-right">
-        <h1>We have Delicious food</h1>
-        <h1>Tasty food in town</h1>
-        <p>
-          Cepteur sint occaecat cupidataf proident, 
-          taken prossession of my entire soul, like 
-          these sweet mornings of spring which I enjoy 
-          with my whole.
-        </p>
       </div>
     </div>
   );
