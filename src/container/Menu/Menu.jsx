dd18d7c306c0efa0c5227e1816bb419d3d49107c
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-08 17:44:18 +0100

    SideItem Implemented

--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -3,17 +3,41 @@ import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { ImCart } from 'react-icons/im';
 
 import './Menu.css';
-import { images } from '../../constants/index';
-// import
+import { SideItem } from '../index';
+import { foods } from '../../data';
 
 
 
 
 const Menu = () => {
-  const [heart, setHeart] = useState(false);
+  const [heart, setHeart] = useState([1, 1]);
+  const [showSideItem, setShowSideItem] = useState(false);
+  const [pickedItem, setpickedItem] = useState(null);
+
+  
+  const R1 = Math.floor(Math.random() * 5);
+  const R2 = Math.floor(Math.random() * 5);
+  
+
+  const addToFav = (x) => {
+    const nextHeart = heart.map((item, i) => {
+      if (i === x) {
+        return !item;
+      } else {
+        return item;
+      }
+    });
+    setHeart(nextHeart);
+  }
 
   return (
     <div id='Menu' className='app__menu'>
+      { showSideItem && 
+        <SideItem 
+          x={ (R1 === pickedItem) ? R1 : R2 } 
+          SideOpen={ setShowSideItem } 
+        /> 
+      }
       <div className="app__menu-right">
         <h1>We have <span>Delicious</span> food</h1>
         <h1><span>Tasty</span> food in town</h1>
@@ -31,18 +55,29 @@ const Menu = () => {
       <div className="app__menu-left">
 
         <div className="app__menu-item">
-          <button ><ImCart className='heart-cart' fontSize={ 22 } /></button>
+          <button
+            onClick={ () => {
+              setShowSideItem(true);
+              setpickedItem(R1);
+              }
+            }
+          >
+            <ImCart
+              className='heart-cart' 
+              fontSize={ 22 } 
+            />
+          </button>
           <div className="app__menu-item-img">
-            <img src={ images.tajine__8 } alt='order' />
+            <img src={ foods[R1].img[0] } alt='order' />
           </div>
           <div className="app__menu-item-info">
             <div className="app__menu-item-name">
-              <h4>Chicken Tajine</h4>
+              <h4>{ foods[R1].name }</h4>
               <small>Medium heat</small>
-              <h5>$24.00</h5>
+              <h5>{ foods[R1].price }</h5>
             </div>
-            <div onClick={ () => setHeart(!heart) } className="app__menu-item-fav">
-            { heart ? <AiOutlineHeart className='app__menu-heart' /> : 
+            <div onClick={ () => addToFav(0) } className="app__menu-item-fav">
+            { heart[0] ? <AiOutlineHeart className='app__menu-heart' /> : 
                 <AiFillHeart className='app__menu-heart' />
               }
             </div>
@@ -50,20 +85,31 @@ const Menu = () => {
         </div>
 
         <div className="app__menu-item">
-          <button ><ImCart className='heart-cart' fontSize={ 22 } /></button>
+          <button
+            onClick={ () => {
+              setShowSideItem(true);
+              setpickedItem(R2);
+              }
+            }
+          >
+            <ImCart
+              className='heart-cart' 
+              fontSize={ 22 } 
+            />
+          </button>
           <div className="app__menu-item-img">
-            <img src={ images.tajine__6 } alt='order' />
+            <img src={ foods[R2].img[0] } alt='order' />
           </div>
           <div className="app__menu-item-info">
             <div className="app__menu-item-name">
-              <h4>Beef Tajine</h4>
+              <h4>{ foods[R2].name }</h4>
               <small>Medium heat</small>
-              <h5>$35.00</h5>
+              <h5>{ foods[R2].price }</h5>
             </div>
-            <div onClick={ () => setHeart(!heart) } className="app__menu-item-fav">
-              { heart ? <AiOutlineHeart className='app__menu-heart' /> : 
+            <div onClick={ () => addToFav(1) } className="app__menu-item-fav">
+              { heart[1] ? <AiOutlineHeart className='app__menu-heart' /> : 
                 <AiFillHeart className='app__menu-heart' />
-              }
+              } 
             </div>
           </div>
         </div>
---
+++ src/container/SideItem/SideItem.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { RiCloseCircleFill } from 'react-icons/ri';
+
+import './SideItem.css';
+import { foods } from '../../data';
+
+
+
+const SideItem = ({ x, SideOpen }) => {
+    const closeToggled = () => {
+        SideOpen(false);
+    }
+
+    return (
+        <div className="app__menu-side-item-main">
+            <div className="app__menu-side-item">
+                <RiCloseCircleFill 
+                    onClick={ () => closeToggled() } 
+                    className='close__side-item' 
+                />
+                <div className='app__menu-side-item-info'>
+                    <h1>{ foods[x].name }</h1>
+                    <label>
+                        <h4>{ foods[x].price }</h4>
+                        <button>Buy</button>
+                    </label>
+                    <div className='app__menu-side-item-wrapper'>
+                        <div className='app__menu-side-item-pics'>
+                            {
+                                foods[x].img.map((image, index) => (
+                                    <img 
+                                        key={ index }
+                                        className='app__site-image' 
+                                        src={ image } 
+                                        alt="food" 
+                                    />
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <h3>
+                        { foods[x].description }
+                    </h3>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SideItem;
